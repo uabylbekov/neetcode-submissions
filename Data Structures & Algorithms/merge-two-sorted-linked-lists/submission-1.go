b6d53c619afaabe8db404e2e184a898c6d79448f
Uluk Abylbekov <uabylbekov@icloud.com>
2026-06-11 22:31:40 -0500

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.go
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	merged := &ListNode{Val: -1}
+	current := merged
+
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+		current = current.Next
+	}
+
+	if list1 == nil {
+		current.Next = list2
+	}
+
+	if list2 == nil {
+		current.Next = list1
+	}
+
+	return merged.Next
+}
