60e7b85194562730fa569b43af2a81e8b3b73921
Uluk Abylbekov <uabylbekov@icloud.com>
2026-06-27 22:36:24 -0500

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.go
@@ -0,0 +1,16 @@
+func searchInsert(nums []int, target int) int {
+	l := 0
+	r := len(nums) - 1
+
+	for l <= r {
+		mid := (l+r) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
+}
